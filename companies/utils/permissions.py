125ef7ecf30263794b142dd03c14257538c7cd47
maichelfreire <jmainfocell@gmail.com>
2024-05-06 22:09:35 -0300

    add permissions

---
+++ companies/utils/permissions.py
@@ -0,0 +1,60 @@
+from rest_framework import permissions
+from accounts.models import UserGroups, GroupPermissons
+from django.contrib.auth.models import Permission
+
+
+def check_permission(user, method, permission_to):
+    
+    if not user.is_authenticated:
+        return False
+    
+    if user.is_owner:
+        return True
+    
+    required_permission = 'view_'+permission_to
+    if method == 'POST':
+        required_permission = 'add_'+permission_to
+    
+    elif method == 'PUT':
+        required_permission = 'change_'+permission_to
+
+    elif method == 'DELETE':
+        required_permission = 'delete_'+permission_to
+
+    groups = UserGroups.objects.values('group_id').filter(user_id=user.id).all()
+
+    for group in groups:
+
+        permissions = GroupPermissons.objects.values('permission_id').filter(group_id=group['group_id']).all()
+
+        for permission in permissions:
+            if Permission.objects.filter(id=permission['permission_id'], codename=required_permission).exists():
+                return True
+            
+
+class EmployeePermission(permissions.BasePermission):
+    message = 'User does not have permission to manage employee'
+
+    def has_permission(self, request, _view):
+        return check_permission(request.user, request.method, permission_to='employee')
+
+
+class GroupPermission(permissions.BasePermission):
+    message = 'User does not have permission to manage groups'
+
+    def has_permission(self, request, _view):
+        return check_permission(request.user, request.method, permission_to='group')
+
+
+class GroupPermissionPermission(permissions.BasePermission):
+    message = 'User does not have permission to manage permissions'
+
+    def has_permission(self, request, _view):
+        return check_permission(request.user, request.method, permission_to='permission')
+    
+
+class TaskPermission(permissions.BasePermission):
+    message = 'User does not have permission to manage tasks'
+
+    def has_permission(self, request, _view):
+        return check_permission(request.user, request.method, permission_to='task')

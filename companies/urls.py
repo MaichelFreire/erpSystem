127d110cbf8175e8b6431d27d4c4079c62f83c90
maichelfreire <jmainfocell@gmail.com>
2024-05-08 22:03:24 -0300

    update view companie

--- companies/urls.py
+++ companies/urls.py
@@ -5,5 +5,5 @@ from companies.views.employees import Employess, EmployeeDetail
 urlpatterns = [
     #  Employee endpoints
     path('employees', Employess.as_view()),
-    path('employess/<int:employee_id>', EmployeeDetail.as_view())
+    path('employees/<int:employee_id>', EmployeeDetail.as_view())
 ]

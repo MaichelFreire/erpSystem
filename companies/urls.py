9e75145a9b61000474bf8a5ef20e5dd7fa47a26c
maichelfreire <jmainfocell@gmail.com>
2024-05-13 21:13:03 -0300

    update views

--- companies/urls.py
+++ companies/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from companies.views.employees import Employess, EmployeeDetail
 from companies.views.permissions import PermissionDetail
+from companies.views.groups import Groups, GroupDetail
 
 urlpatterns = [
     #  Employee endpoints
@@ -9,5 +10,7 @@ urlpatterns = [
     path('employees/<int:employee_id>', EmployeeDetail.as_view()),
 
     #  Groups And Permissions Endpoints
+    path("groups", Groups.as_view()),
+    path("groups/<int:group_id>", GroupDetail.as_view()),
     path('permissions', PermissionDetail.as_view()),
 ]
